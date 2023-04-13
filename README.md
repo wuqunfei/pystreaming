413a6a71919807e4ae1af3231422490ce2606853
Qunfei Wu <wu.qunfei@gmail.com>
2023-04-13 17:23:24 +0200

    adding first hello world example

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# pystreaming
+# Pystreaming
 A python streaming learning material 
+
+## Install Kafka (redpanda) 
+```shell
+docker compose up -d
+brew install redpanda-data/tap/redpanda
+rpk cluster info --brokers 127.0.0.1:19092
+```
---
+++ pystreaming/hello_world.py
@@ -0,0 +1,31 @@
+import logging
+import faust
+import datetime
+from faust import Worker
+
+app = faust.App(
+    id='hello_world',  # application id
+    broker='kafka://127.0.0.1:19092',
+    value_serializer='raw',
+    store='rocksdb://',
+    version=1,
+)
+
+greetings_topic = app.topic('greetings')
+
+
+@app.agent(greetings_topic)
+async def consumer(greetings):
+    async for greeting in greetings:
+        app.logger.info(str(greeting, 'utf-8'))
+
+
+@app.timer(5)
+async def producer():
+    now = datetime.datetime.now()
+    await greetings_topic.send(value=f'hello, stream {now}')
+
+
+if __name__ == '__main__':
+    worker = Worker(app=app, loglevel=logging.INFO, debug=True)
+    worker.execute_from_commandline()
